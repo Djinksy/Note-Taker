a49b2bfff4c05b8afa13574ed9ebf4e463cff095
Djinksy <damienjinks39@gmail.com>
2022-05-26 17:18:06 +1000

    server.js/ added Route files

--- Develop/server.js
+++ Develop/server.js
@@ -11,24 +11,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-//GET Route for public index
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-//GET Route for public notes
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-// GET Route for * to return public index
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+//Creating routes to api/html files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
-});
-
+//app listening/ starts localhost server
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
 });
